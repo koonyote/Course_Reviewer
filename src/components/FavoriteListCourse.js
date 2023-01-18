0ec6ab661cae481464f940793356af0175f11580
koonyote <koonyote@gmail.com>
2023-01-18 17:32:19 +0700

    profile

--- src/components/FavoriteListCourse.js
+++ src/components/FavoriteListCourse.js
@@ -2,10 +2,7 @@ import * as React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery"; // ตัวจัด Fullscreen
 import config from "../config.json";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
@@ -23,19 +20,17 @@ import IconButton from '@mui/material/IconButton';
 const theme = createTheme();
 const theme_favorite = createTheme({
     typography: {
-      button: {
-        fontSize: "1rem",
-      },
+        button: {
+            fontSize: "1rem",
+        },
     },
-  });
-
-
-
+});
 
 export default function FavoriteList() {
     const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
     const token = localStorage.getItem("token");
-    const [apiData,setApiData] = React.useState()
+    const [apiData, setApiData] = React.useState()
+    const [effect, setEffect] = React.useState(0)
     React.useEffect(() => {
         const api = async () => {
             const token = localStorage.getItem("token");
@@ -59,112 +54,112 @@ export default function FavoriteList() {
         window.setTimeout(() => {
             api();
         }, 1000);
-    }, [apiData] );
+    }, [effect]);
 
-    async function API_Add_Favorite(code) {
+    async function API_Delete_Favorite(code) {
         const send = await fetch(`${config.domain}/delete-favorite`, {
-          method: "DELETE",
-          headers: {
-            "Authorization": `Bearer ${token}`,
-            "ngrok-skip-browser-warning": "*",
-            "User-Agent": "Custom",
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({
-            course_id: code,
-          }),
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "ngrok-skip-browser-warning": "*",
+                "User-Agent": "Custom",
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                course_id: code,
+            }),
         });
         const status = send.status === 200 ? true : false;
         return status
-      }
-    
-      async function handle_bt_favorite(event, course_id) {
-        console.log(course_id)
-        const send = await API_Add_Favorite(course_id)
-        if (send) setApiData(undefined)
-      }
+    }
+
+    async function handle_bt_favorite(event, course_id) {
+        const send = await API_Delete_Favorite(course_id)
+        if (send) setEffect(effect + 1)
+    }
 
     return (
-        apiData  ?  
-        <Grid container spacing={4}>
-        {apiData.map((data) => (
-          <Grid item key={data} xs={12} sm={6} md={4}>
-            <Card
-              sx={{
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                shadows: 1,
-              }}
-            >
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="h2"
-                  textAlign={"center"}
-                  href="#"
-                >
-                  <Link
-                    href={`/course-detail/${data.course_id}`}
-                    underline="hover"
-                  >
-                    {data.course_id}
-                  </Link>
-                </Typography>
-                <Typography textAlign={"center"}>
-                  {" "}
-                  {data.course_name_th}{" "}
-                </Typography>
-                <Typography textAlign={"center"}>
-                  {" "}
-                  {data.course_name_en}{" "}
-                </Typography>
-              </CardContent>
-              <CardActions sx={{ justifyContent: "center" }}>
-                <ThemeProvider theme={theme_favorite}>
-                 {/* Icon */}
-                    <IconButton sx={{ color: "#FFC0CB" , 
-                ":hover": {
-                    backgroundColor: "#fffff",
-                  },
-                  }}
-                  onClick={(e) => {
-                    handle_bt_favorite(e, data.course_id);
-                  }}>
-                        <FavoriteIcon/>
-                    </IconButton>
-                </ThemeProvider>
-              </CardActions>
-              <CardActions sx={{ justifyContent: "center" }}>
-                <Button
-                  size="small"
-                  onClick={() => window.open(`/rating/${data.course_id}`)}
-                >
-                  <Rating
-                    name="half-rating-read"
-                    size="small"
-                    defaultValue={data.score}
-                    precision={0.5}
-                    readOnly
-                    sx={{ paddingBottom: 0.5 }}
-                  />
-                </Button>
-                <Button
-                  size="small"
-                  sx={{ alignContent: "center" }}
-                  onClick={() => window.open(`/comment/${data.course_id}`)}
-                >
-                  <CommentIcon fontSize="small"></CommentIcon> &nbsp; (
-                  {data.comment})
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      : <LinearProgress />
+        apiData ?
+            <Grid container spacing={4}>
+                {apiData.map((data) => (
+                    <Grid item key={data} xs={12} sm={6} md={4}>
+                        <Card
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                shadows: 1,
+                            }}
+                        >
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="h2"
+                                    textAlign={"center"}
+                                    href="#"
+                                >
+                                    <Link
+                                        href={`/course-detail/${data.course_id}`}
+                                        underline="hover"
+                                    >
+                                        {data.course_id}
+                                    </Link>
+                                </Typography>
+                                <Typography textAlign={"center"}>
+                                    {" "}
+                                    {data.course_name_th}{" "}
+                                </Typography>
+                                <Typography textAlign={"center"}>
+                                    {" "}
+                                    {data.course_name_en}{" "}
+                                </Typography>
+                            </CardContent>
+                            <CardActions sx={{ justifyContent: "center" }}>
+                                <ThemeProvider theme={theme_favorite}>
+                                    {/* Icon */}
+                                    <IconButton sx={{
+                                        color: "#FFC0CB",
+                                        ":hover": {
+                                            backgroundColor: "#fffff",
+                                        },
+                                    }}
+                                        onClick={(e) => {
+                                            handle_bt_favorite(e, data.course_id);
+                                        }}>
+                                        <FavoriteIcon />
+                                    </IconButton>
+                                </ThemeProvider>
+                            </CardActions>
+                            <CardActions sx={{ justifyContent: "center" }}>
+                                <Button
+                                    size="small"
+                                    onClick={() => window.open(`/rating/${data.course_id}`)}
+                                >
+                                    <Rating
+                                        name="half-rating-read"
+                                        size="small"
+                                        defaultValue={data.score}
+                                        precision={0.5}
+                                        readOnly
+                                        sx={{ paddingBottom: 0.5 }}
+                                    />
+                                </Button>
+                                <Button
+                                    size="small"
+                                    sx={{ alignContent: "center" }}
+                                    onClick={() => window.open(`/comment/${data.course_id}`)}
+                                >
+                                    <CommentIcon fontSize="small"></CommentIcon> &nbsp; (
+                                    {data.comment})
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+            : <LinearProgress />
     );
 }
