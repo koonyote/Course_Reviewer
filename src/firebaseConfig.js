cc8864fb17428d156b8a8034df476b36f0573490
koonyote <koonyote@gmail.com>
2023-01-27 14:53:51 +0700

    firebase key

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDRVXWLSz006JDqpe1OqZOLVPoziYYBLoc",
+    authDomain: "course-reviewer-53b18.firebaseapp.com",
+    projectId: "course-reviewer-53b18",
+    storageBucket: "course-reviewer-53b18.appspot.com",
+    messagingSenderId: "953013270613",
+    appId: "1:953013270613:web:86fcec58059cf707277cdc",
+  };
