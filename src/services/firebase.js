dcacdf53058106820824ed5fec035e0f940f3dbc
koonyote <koonyote@gmail.com>
2023-02-07 17:17:15 +0700

    firebase debug

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,34 +1,40 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { firebaseConfig } from "../firebaseConfig";
-import React, { useEffect } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup, getRedirectResult, signInWithRedirect } from "firebase/auth";
 import config from "../config.json";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseSDK = {
+  apiKey: "AIzaSyDRVXWLSz006JDqpe1OqZOLVPoziYYBLoc",
+  authDomain: "course-reviewer-53b18.firebaseapp.com",
+  projectId: "course-reviewer-53b18",
+  storageBucket: "course-reviewer-53b18.appspot.com",
+  messagingSenderId: "953013270613",
+  appId: "1:953013270613:web:86fcec58059cf707277cdc",
+};
 
+const app = initializeApp(firebaseSDK);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+
 export const GetCurrentUeser = () => {
   getAuth().currentUser.getIdToken(true)
-  .then(function(idToken) {
-    console.log("Get Current User: getIdToken OK!")
-  // console.log(idToken)
-  localStorage.setItem("token",idToken)
-  }).catch(function(error) {
-    console.log(error)
-    alert('Error, refresh token.')
-  });
+    .then(function (idToken) {
+      console.log("Get Current User: getIdToken OK!")
+      localStorage.setItem("token", idToken)
+    }).catch(function (error) {
+      console.log(error)
+      alert('Error, refresh token.')
+    });
 }
 
 export const signInWithGoogle = () => {
-  if (localStorage.getItem("email") !== null && localStorage.getItem("isMember") == true  ) {
-    console.log('check')
+  if (localStorage.getItem("email") !== null && localStorage.getItem("isMember") == true) {
+    // window.location.href = `${config.prefix_server}/home`;
     window.location.href = "/home";
-  } else if (localStorage.getItem("isMember") == false ) {
-    window.location.href = "/register"
-  } 
-  
+  } else if (localStorage.getItem("isMember") == false) {
+    window.location.href = "/register";
+    // window.location.href = `${config.prefix_server}/register`
+  }
+
   async function call_api_check_member(token) {
     const API = await fetch(`${config.domain}/login`, {
       method: "POST",
@@ -39,19 +45,20 @@ export const signInWithGoogle = () => {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Access-Control-Allow-Origin": "*",
-    },
-      // body : {}
+      }
     });
     const jsonData = await API.json();
 
     if (jsonData.isMember) {
+      // window.location.href = `${config.prefix_server}/home`;
       window.location.href = "/home";
-      // console.log(jsonData)
-      localStorage.setItem("isMember",true)
-      localStorage.setItem("api_expire",jsonData.expire_th)
+      localStorage.setItem("isMember", true)
+      localStorage.setItem("api_expire", jsonData.expire_th)
+      localStorage.setItem("Role", jsonData.permission)
       console.log('isMember')
     } else {
-      localStorage.setItem("isMember",false)
+      localStorage.setItem("isMember", false)
+      // window.location.href = `${config.prefix_server}/register`
       window.location.href = "/register";
     }
   }
@@ -63,14 +70,24 @@ export const signInWithGoogle = () => {
       const profilePic = result.user.photoURL;
       const token = result.user.accessToken;
       const expTimestamp = result.user.stsTokenManager.expirationTime;
-      // console.log(token);
+
       localStorage.setItem("expTimestamp", expTimestamp);
       localStorage.setItem("expDateTime", new Date(expTimestamp).toLocaleString());
       localStorage.setItem("name", name);
       localStorage.setItem("email", email);
       localStorage.setItem("profilePic", profilePic);
-      localStorage.setItem("token",token)
+      localStorage.setItem("token", token)
       call_api_check_member(token);
     })
     .catch((error) => console.log(error));
 };
+
+export const SignOut = () => {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    console.log("Sign-out successful")
+  }).catch((error) => {
+    // An error happened.
+    console.log(error)
+  });
+}
